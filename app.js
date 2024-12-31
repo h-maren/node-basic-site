f41ca0ca496a4377ffdbe6569f448d3a91e328f9
h-maren <26232423+h-maren@users.noreply.github.com>
2024-12-31 14:38:49 -0500

    Add links to pages, routing to all but custom 404 Both index and / go to index pages About and contact links and routing added Need to add custom 404 page

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var url=require('url');
 var fs=require('fs');
 
 http.createServer(function(req,res){
-    var q=url.parse(req.url,true);
-    var filename="."+q.pathname;
-    fs.readFile(filename,function(err,data){
-        if(err) {
-            res.writeHead(404,{'Content-Type':'text/html'});
-            return res.end('404 Not Found');
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
+    if(req.url=="/index"||req.url=="/"){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        var readStream=fs.createReadStream('index.html');
+        readStream.pipe(res)
+    } else {
+        var q=url.parse(req.url,true);
+        var filename="."+q.pathname;
+        fs.readFile(filename,function(err,data){
+            if(err) {
+                res.writeHead(404,{'Content-Type':'text/html'});
+                return res.end('404 Not Found');
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        })
+    }
 }).listen(8080);
