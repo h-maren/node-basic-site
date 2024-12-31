fe63d42039d2cadd5a5adea332a9bb569428458a
h-maren <26232423+h-maren@users.noreply.github.com>
2024-12-31 14:52:46 -0500

    Add redirect to custom 404 page

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ http.createServer(function(req,res){
         var filename="."+q.pathname;
         fs.readFile(filename,function(err,data){
             if(err) {
-                res.writeHead(404,{'Content-Type':'text/html'});
-                return res.end('404 Not Found');
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                var errorPage=fs.readFileSync('404.html');
+                res.write(errorPage);
+                return res.end();
+
             }
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data);
